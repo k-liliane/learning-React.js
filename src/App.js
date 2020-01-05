20ff581533b8c244e93a4dfdb0c56e9ad6501708
k-liliane <liliane.kenmogne@yahoo.com>
2020-01-05 14:13:34 +0100

    introduction au jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const  name = 'tom';
+    const text = <p> lili berca bien chez notre oncle francois nestor</p>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Notre Composant</h1>
+      <div> HELLO  {name}!!
+        <h1> 1+1 = {1+1}</h1>
+        <h2> texte : {text} </h2>
+        <input type="button" value="ok"/>
+      </div>
     </div>
   );
 }
